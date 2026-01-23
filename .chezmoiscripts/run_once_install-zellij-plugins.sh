1b40a7f19d80ec070aea8fe7285ec7fa0fce961d
Shin'ichiro Suzuki <shin@sforzando.co.jp>
2026-01-23 22:05:56 +0900

    Add .chezmoiscripts/install-zellij-plugins.sh Update .vscode/settings.json Update .config/zellij/config.kdl

---
+++ .chezmoiscripts/run_once_install-zellij-plugins.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+
+echo "Installing Zellij plugins..."
+
+# Create plugins directory
+mkdir -p ~/.config/zellij/plugins
+
+# Download zsm plugin
+echo "Downloading zsm plugin..."
+curl -L https://github.com/liam-mackie/zsm/releases/latest/download/zsm.wasm \
+  -o ~/.config/zellij/plugins/zsm.wasm
+
+echo "Zellij plugins installed successfully."
