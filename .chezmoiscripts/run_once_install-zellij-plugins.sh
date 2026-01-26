69e6a41aa13e1f41b1e3b8c01804d4e0ae6d01b3
Shin'ichiro Suzuki <shin@sforzando.co.jp>
2026-01-27 00:23:53 +0900

    feat: :sparkles: add Zoxide Session Manager plugin and new layouts

--- .chezmoiscripts/run_once_install-zellij-plugins.sh
+++ .chezmoiscripts/run_once_install-zellij-plugins.sh
@@ -7,7 +7,7 @@ echo "Installing Zellij plugins..."
 # Create plugins directory
 mkdir -p ~/.config/zellij/plugins
 
-# Download zsm plugin
+# Download Zoxide Session Manager plugin
 echo "Downloading zsm plugin..."
 curl -L https://github.com/liam-mackie/zsm/releases/latest/download/zsm.wasm \
   -o ~/.config/zellij/plugins/zsm.wasm
